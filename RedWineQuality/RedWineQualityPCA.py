82ba0e3dc616415c476d72751079ff242a812406
pranavj1001 <pranavj1001@gmail.com>
2018-02-04 12:45:57 +0530

    add RedWineQualityPCA.py

---
+++ RedWineQuality/RedWineQualityPCA.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  4 12:24:14 2018
+
+@author: pranavjain
+
+"""
+
+# Importing the libraries
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('winequality-red.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 11].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = None)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
