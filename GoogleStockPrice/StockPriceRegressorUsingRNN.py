308519f7fc35dea0b871727062b3f226010b31de
pranavj1001 <pranavj1001@gmail.com>
2018-02-11 15:40:19 +0530

    feature scale the training data
    also make it a 3D array

--- GoogleStockPrice/StockPriceRegressorUsingRNN.py
+++ GoogleStockPrice/StockPriceRegressorUsingRNN.py
@@ -14,3 +14,15 @@ import pandas as pd
 # Importing the training set
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Making inputs and outputs
+X_train = training_set_scaled[0:1257]
+y_train = training_set_scaled[1:1258]
+
+# reshaping X_train
+X_train = np.reshape(X_train, (1257, 1, 1))
